56b94f83bdd18ed01d6e33c690ef2f7695ec1078
Elias P. Pereira <eliaspinheiropereiraa@gmail.com>
2026-07-30 18:34:55 -0300

    iniciando o projeto

---
+++ src/main/java/io/gitub/eliaspinheiropereira/speed_violation_service/SpeedViolationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.gitub.eliaspinheiropereira.speed_violation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedViolationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedViolationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gitub/eliaspinheiropereira/speed_violation_service/SpeedViolationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitub.eliaspinheiropereira.speed_violation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedViolationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
